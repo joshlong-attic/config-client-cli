950e882cc1df985657a1c12db81cb38a23424d1b
Josh Long <josh@joshlong.com>
2020-11-03 04:43:21 -0800

    polish

--- src/main/java/com/example/configclient/ConfigClientApplication.java
+++ src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -3,6 +3,8 @@ package com.example.configclient;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,8 +18,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 import java.io.*;
 import java.util.*;
 
+
 @SpringBootApplication(proxyBeanMethods = false)
 public class ConfigClientApplication {
+
+	private final Log log = LogFactory.getLog(getClass());
 
 	public static void main(String[] args) {
 		var context = new SpringApplicationBuilder()
@@ -26,50 +31,56 @@ public class ConfigClientApplication {
 			.run(args);
 	}
 
+	private final String usage = "Usage: gacc <CS_USER> <CS_PW> <APP_NAME> <PROFILE> <OUTPUT_PATH> " ;
+
 	@Bean
 	CommandLineRunner ready() {
 		return args -> {
-			var index = 0;
-			var username = args[index++];
-			var pw = args[index++];
-			var springApplicationName = args[index++];
-			var profile = args[index++];
-			var configServerUri = args[index++];
-			var outputPath = args[index++];
+			try {
+				var index = 0;
+				var username = args[index++];
+				var pw = args[index++];
+				var springApplicationName = args[index++];
+				var profile = args[index++];
+				var configServerUri = args[index++];
+				var outputPath = args[index++];
 
-			var http = WebClient.builder()
-				.filter(ExchangeFilterFunctions.basicAuthentication(username, pw))
-				.build();
-			var url = configServerUri + '/' + springApplicationName + '/' + profile;
-			var map = http.get().uri(url).retrieve().bodyToMono(JsonNode.class)
-				.map(body -> {
-					var propertySources = (ArrayNode) body.get("propertySources");
-					System.out.println("not sure what kind of propertySources i have now");
-					var maps = new ArrayList<Map<String, String>>();
-					propertySources.elements().forEachRemaining(json -> maps.add(mapOfConfigurationFrom(json)));
-					Collections.reverse(maps);
-					var toWriteOut = new HashMap<String, String>();
-					maps.stream().forEachOrdered(toWriteOut::putAll);
-					return toWriteOut;
-				});
-			var results = map.block();
-			var existingContent = new StringBuilder();
-			var file = new File(outputPath);
-			try (var fr = new BufferedReader(new FileReader(file))) {
-				existingContent.append(FileCopyUtils.copyToString(fr));
-				existingContent.append(System.lineSeparator());
+				var http = WebClient.builder()
+					.filter(ExchangeFilterFunctions.basicAuthentication(username, pw))
+					.build();
+				var url = configServerUri + '/' + springApplicationName + '/' + profile;
+				var map = http.get().uri(url).retrieve().bodyToMono(JsonNode.class)
+					.map(body -> {
+						var propertySources = (ArrayNode) body.get("propertySources");
+						var maps = new ArrayList<Map<String, String>>();
+						propertySources.elements().forEachRemaining(json -> maps.add(mapOfConfigurationFrom(json)));
+						Collections.reverse(maps);
+						var toWriteOut = new HashMap<String, String>();
+						maps.stream().forEachOrdered(toWriteOut::putAll);
+						return toWriteOut;
+					});
+				var results = map.block();
+				var existingContent = new StringBuilder();
+				var file = new File(outputPath);
+				try (var fr = new BufferedReader(new FileReader(file))) {
+					existingContent.append(FileCopyUtils.copyToString(fr));
+					existingContent.append(System.lineSeparator());
+				}
+				try (var fw = new BufferedWriter(new FileWriter(file))) {
+					fw.write(existingContent.toString());
+					Objects.requireNonNull(results).forEach((key, value) -> {
+						var newLine = key + "=" + value + System.lineSeparator();
+						try {
+							fw.write(newLine);
+						}
+						catch (IOException e) {
+							ReflectionUtils.rethrowRuntimeException(e);
+						}
+					});
+				}
 			}
-			try (var fw = new BufferedWriter(new FileWriter(file))) {
-				fw.write(existingContent.toString());
-				Objects.requireNonNull(results).forEach((key, value) -> {
-					var newLine = key + "=" + value + System.lineSeparator();
-					try {
-						fw.write(newLine);
-					}
-					catch (IOException e) {
-						ReflectionUtils.rethrowRuntimeException(e);
-					}
-				});
+			catch (Exception ex) {
+				log.error( "There was an issue when trying to use the program!" + System.lineSeparator() + this.usage , ex);
 			}
 		};
 	}
