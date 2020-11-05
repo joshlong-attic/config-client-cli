804f9654b1c29d072b02e8e0a298e616b40a2681
Josh Long <josh@joshlong.com>
2020-11-05 00:40:05 -0800

    config

--- src/main/java/com/example/configclient/ConfigClientApplication.java
+++ src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -14,8 +14,10 @@ import org.springframework.util.FileCopyUtils;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.util.retry.Retry;
 
 import java.io.*;
+import java.time.Duration;
 import java.util.*;
 
 
@@ -36,58 +38,82 @@ public class ConfigClientApplication {
 	@Bean
 	CommandLineRunner ready() {
 		return args -> {
-			try {
-				var index = 0;
-				var username = args[index++];
-				var pw = args[index++];
-				var springApplicationName = args[index++];
-				var profile = args[index++];
-				var configServerUri = args[index++];
-				var outputPath = args[index++];
 
-				var http = WebClient.builder()
-					.filter(ExchangeFilterFunctions.basicAuthentication(username, pw))
-					.build();
-				var url = configServerUri + '/' + springApplicationName + '/' + profile;
-				var map = http.get().uri(url).retrieve().bodyToMono(JsonNode.class)
-					.map(body -> {
-						var propertySources = (ArrayNode) body.get("propertySources");
-						var maps = new ArrayList<Map<String, String>>();
-						propertySources.elements().forEachRemaining(json -> maps.add(mapOfConfigurationFrom(json)));
-						Collections.reverse(maps);
-						var toWriteOut = new HashMap<String, String>();
-						maps.stream().forEachOrdered(toWriteOut::putAll);
-						return toWriteOut;
-					});
-				var results = map.block();
-				var existingContent = new StringBuilder();
-				var file = new File(outputPath);
+			var index = 0;
+			var username = args[index++];
+			var pw = args[index++];
+			var springApplicationName = args[index++];
+			var profile = args[index++];
+			var configServerUri = args[index++];
+			var outputPath = args[index++];
 
-				if (file.exists()) {
-					try (var fr = new BufferedReader(new FileReader(file))) {
-						existingContent.append(FileCopyUtils.copyToString(fr));
-						existingContent.append(System.lineSeparator());
-					}
+
+			var http = WebClient.builder()
+				.filter(ExchangeFilterFunctions.basicAuthentication(username, pw))
+				.build();
+			var url = configServerUri + '/' + springApplicationName + '/' + profile;
+
+
+			var maxTries = 100;
+			var ctr = 0;
+			var continueCalls = true;
+			while (ctr++ < maxTries && continueCalls) {
+				try {
+
+					this.attemptCall(outputPath, http, url);
+					continueCalls = false;
 				}
+				catch (Exception ex) {
+					log.error("There was an issue when trying to use the program!"
+						+ System.lineSeparator() + this.usage, ex);
 
-
-				try (var fw = new BufferedWriter(new FileWriter(file))) {
-					fw.write(existingContent.toString());
-					Objects.requireNonNull(results).forEach((key, value) -> {
-						var newLine = key + "=" + value + System.lineSeparator();
-						try {
-							fw.write(newLine);
-						}
-						catch (IOException e) {
-							ReflectionUtils.rethrowRuntimeException(e);
-						}
-					});
 				}
-			}
-			catch (Exception ex) {
-				log.error("There was an issue when trying to use the program!" + System.lineSeparator() + this.usage, ex);
 			}
 		};
+	}
+
+	private void attemptCall(String outputPath, WebClient http, String url) throws IOException {
+		var map = http
+			.get()//
+			.uri(url)//
+			.retrieve() //
+			.bodyToMono(JsonNode.class)//
+			.retryWhen(Retry.backoff(100, Duration.ofSeconds(2)))//
+			.map(body -> {
+				var propertySources = (ArrayNode) body.get("propertySources");
+				var maps = new ArrayList<Map<String, String>>();
+				propertySources.elements().forEachRemaining(json -> maps.add(mapOfConfigurationFrom(json)));
+				Collections.reverse(maps);
+				var toWriteOut = new HashMap<String, String>();
+				maps.stream().forEachOrdered(toWriteOut::putAll);
+				return toWriteOut;
+			});
+		var results = map.block();
+
+		var existingContent = new StringBuilder();
+		var file = new File(outputPath);
+
+		if (file.exists()) {
+			try (var fr = new BufferedReader(new FileReader(file))) {
+				existingContent.append(FileCopyUtils.copyToString(fr));
+				existingContent.append(System.lineSeparator());
+			}
+		}
+
+
+		try (var fw = new BufferedWriter(new FileWriter(file))) {
+			fw.write(existingContent.toString());
+			Objects.requireNonNull(results).forEach((key, value) -> {
+				var newLine = key + "=" + value + System.lineSeparator();
+				try {
+					log.info("found " + key);
+					fw.write(newLine);
+				}
+				catch (IOException e) {
+					ReflectionUtils.rethrowRuntimeException(e);
+				}
+			});
+		}
 	}
 
 	private Map<String, String> mapOfConfigurationFrom(JsonNode jsonNode) {
